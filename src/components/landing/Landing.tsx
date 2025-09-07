3d10ef4db30e0fef0ff7bd0bc7f864f439f0a757
JVillo28 <jaimevvvmmm@gmail.com>
2025-09-07 17:39:18 +0200

    landing responsive

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -8,7 +8,7 @@ export default  function Landing() {
   return(
     <>
         <Navbar />
-        <div id="container" className="flex flex-row justify-center" >
+        <div id="container" className="flex flex-col md:flex-row justify-center items-stretch" >
           <BigCard 
             title="Calculadora de transportes"
             imageSrc="/busLanding.png" 
@@ -16,8 +16,8 @@ export default  function Landing() {
             imageHeight={845}
             imageWidth={600}
             toRedirect="/calculadoraTransportes"
-            firstParagraph="La Dirección General de Transportes y Movilidad dota a las empresas del sector de una herramienta que les permita, de una manera rápida y sencilla, conocer tanto los costes unitarios por vehículo como los costes totales de los servicios que realicen o se planteen realizar. "
-            secondParagraph="En esta versión se recogen los datos obtenidos en la DECIMOSEXTA ACTUALIZACIÓN del Observatorio de Costes del Transporte de Mercancías por Carretera en la Comunidad de Madrid."
+            firstParagraph="La Dirección General de Transportes y Movilidad dota a las empresas del sector de una herramienta que les permita, de una manera rápida y sencilla, conocer tanto los costes unitarios por vehículo como los costes totales de los servicios que realicen o se planteen realizar. "
+            secondParagraph="En esta versión se recogen los datos obtenidos en la DECIMOSEXTA ACTUALIZACIÓN del Observatorio de Costes del Transporte de Mercancías por Carretera en la Comunidad de Madrid."
             buttonText="Acceder a la calculadora de transporte"
             />
           <BigCard 
@@ -26,8 +26,8 @@ export default  function Landing() {
             imageAlt="truckLanding"
             imageHeight={845}
             imageWidth={600}
-            firstParagraph="La Dirección General de Transportes pone a disposición de las empresas del sector la aplicación informática que sirve de herramienta de ayuda a los empresarios para calcular tanto los costes unitarios por vehículo como por servicios o trayectos concretos, según el tipo de vehículo utilizado para su realización. "
-            secondParagraph="En esta versión se recogen los datos obtenidos en la DUODÉCIMA ACTUALIZACIÓN del Observatorio de Costes del Transporte Discrecional de Viajeros en Autobús en la Comunidad de Madrid. "
+            firstParagraph="La Dirección General de Transportes pone a disposición de las empresas del sector la aplicación informática que sirve de herramienta de ayuda a los empresarios para calcular tanto los costes unitarios por vehículo como por servicios o trayectos concretos, según el tipo de vehículo utilizado para su realización. "
+            secondParagraph="En esta versión se recogen los datos obtenidos en la DUODÉCIMA ACTUALIZACIÓN del Observatorio de Costes del Transporte Discrecional de Viajeros en Autobús en la Comunidad de Madrid. "
             toRedirect="/calculadoraMercancias"
             buttonText="Acceder a la calculadora de mercancías"
           />
@@ -35,4 +35,4 @@ export default  function Landing() {
         </div>
     </>
   )
-}   
+}
--- src/components/landing/components/BigCard.tsx
+++ src/components/landing/components/BigCard.tsx
@@ -19,9 +19,9 @@ interface Props {
 export default function BigCard({title,imageSrc, imageAlt, imageWidth, imageHeight,firstParagraph,secondParagraph,toRedirect,buttonText}: Props) {
     const router = useRouter();
     return(
-        <div className="w-3xl mx-3 my-6 bg-white rounded-4xl hover:scale-101 transition-all duration-300 shadow-lg hover:shadow-2xl hover:cursor-pointer" onClick={()=>router.push(toRedirect)}>  
-            <div id="container" className="flex flex-col justify-center items-center px-10 border-1 border-gray-300 rounded-4xl">
-                <h1 id="calcType" className="text-[1.8rem] font-bold my-10">{title}</h1>
+        <div className="w-full md:max-w-xl h-full mx-auto md:mx-2 my-6 bg-white rounded-4xl hover:scale-101 transition-all duration-300 shadow-lg hover:shadow-2xl hover:cursor-pointer flex flex-col" onClick={()=>router.push(toRedirect)}>  
+            <div id="container" className="flex flex-col justify-between items-center px-4 sm:px-6 md:px-10 border-1 border-gray-300 rounded-4xl h-full">
+                <h1 id="calcType" className="text-xl sm:text-2xl md:text-[1.8rem] font-bold my-5 md:my-10 text-center">{title}</h1>
                 <Image 
                     src={imageSrc} 
                     alt={imageAlt} 
@@ -29,7 +29,7 @@ export default function BigCard({title,imageSrc, imageAlt, imageWidth, imageHeig
                     height={imageHeight}
                     className="rounded-4xl w-full"
                     />
-                <div className="flex flex-col justify-center text-center text-[0.9rem] my-5 font-medium">
+                <div className="flex flex-col justify-center text-center text-sm md:text-[0.95rem] text-[0.65rem] my-4 md:my-5 font-medium h-[150px] md:h-[250px]  ">
                     <p className="">
                         {firstParagraph}
                     </p>
@@ -38,8 +38,8 @@ export default function BigCard({title,imageSrc, imageAlt, imageWidth, imageHeig
                         {secondParagraph}
                     </p>
                 </div>
-                <div className="my-8">
-                    <Button className="bg-red text-white w-2xl rounded-4xl " onClick={()=>router.push(toRedirect)}>{buttonText}</Button>
+                <div className="my-5 md:my-8 w-full flex justify-center">
+                    <Button className="bg-red text-white w-full sm:w-auto max-w-[300px] rounded-4xl px-4 py-2" onClick={()=>router.push(toRedirect)}>{buttonText}</Button>
                 </div>
                 
             </div>
